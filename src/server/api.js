53377896924f05fa27cfbc2daf91bea643e8e5b9
elma narimani <elma@dotical.org>
2024-04-06 05:59:21 +0330

    Using axios instead of fetch

--- src/server/api.js
+++ src/server/api.js
@@ -1,31 +1,33 @@
+import axios from "axios";
+
+const URL = "/api/jobs";
+
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
 // Add new job
 export const addJob = async (newJob) => {
-  const res = await fetch("/api/jobs", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newJob),
-  });
-  return;
-};
-
-// Delete job
-export const deleteJob = async (id) => {
-  const res = await fetch(`/api/jobs/${id}`, {
-    method: "DELETE",
-  });
-  return;
+  try {
+    const response = await axios.post(URL, newJob);
+  } catch (error) {
+    console.log(error.response);
+  }
 };
 
 // Update job
 export const updateJob = async (job) => {
-  const res = await fetch(`/api/jobs/${job.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(job),
-  });
-  return;
+  try {
+    const response = await axios.put(URL + `/${job.id}`, job);
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+// Delete job
+export const deleteJob = async (id) => {
+  try {
+    const response = await axios.delete(URL + `/${id}`, id);
+  } catch (error) {
+    console.log(error.response);
+  }
 };
