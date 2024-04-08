110bdddff52577cd4a155e32d9275e6fd11e3c34
elma narimani <elma@dotical.org>
2024-04-08 11:47:32 +0330

    using useFormik hook

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,37 +1,47 @@
 import { useParams, useLoaderData, Link, useNavigate } from 'react-router-dom';
-import { useState } from "react";
+import { useFormik } from 'formik';
+import { jobSchema } from '../schemas';
 import { toast } from 'react-toastify';
 
 const EditJobPage = ({ updateJobSubmit }) => {
   const job = useLoaderData();
-  const [type, setType] = useState(job.type);
-  const [title, setTitle] = useState(job.title);
-  const [description, setDescription] = useState(job.description);
-  const [location, setLocation] = useState(job.location);
-  const [salary, setSalary] = useState(job.salary);
-  const [companyName, setCompanyName] = useState(job.company.name);
-  const [companyDescription, setCompanyDescription] = useState(job.company.description);
-  const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
-  const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
+
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      type: job.type,
+      title: job.title,
+      description: job.description,
+      location: job.location,
+      salary: job.salary,
+      companyName: job.company.name,
+      companyDescription: job.company.description,
+      contactEmail: job.company.contactEmail,
+      contactPhone: job.company.contactPhone,
+    },
+    validationSchema: jobSchema,
+    onSubmit: (values, actions) => {
+      submitForm(values);
+      actions.resetForm();
+    },
+  })
 
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const submitForm = (e) => {
-    e.preventDefault();
+  const submitForm = (values) => {
 
     const updatedJob = {
-      id,
-      type,
-      title,
-      description,
-      location,
-      salary,
+      id: id,
+      type: values.type,
+      title: values.title,
+      description: values.description,
+      location: values.location,
+      salary: values.salary,
       company: {
-        name: companyName,
-        description: companyDescription,
-        contactEmail,
-        contactPhone
+        name: values.companyName,
+        description: values.companyDescription,
+        contactEmail: values.contactEmail,
+        contactPhone: values.contactPhone
       }
     }
 
@@ -42,77 +52,79 @@ const EditJobPage = ({ updateJobSubmit }) => {
     navigate(`/jobs/${id}`)
   }
 
+  const classInput = errors.title && touched.title ? "border border-red-500 rounded w-full py-2 px-3 mb-2" : 'border rounded w-full py-2 px-3 mb-2'
+
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form onSubmit={submitForm}>
+          <form onSubmit={handleSubmit}>
             <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
-              >Job Type</label
-              >
+              >Job Type</label>
               <select
                 id="type"
-                name="type"
-                className="border rounded w-full py-2 px-3"
+                name="type" className="border rounded w-full py-2 px-3"
                 required
-                value={type}
-                onChange={(e) => setType(e.target.value)}
+                value={values.type}
+                onChange={handleChange}
+                onBlur={handleBlur}
               >
                 <option value="Full-Time">Full-Time</option>
                 <option value="Part-Time">Part-Time</option>
                 <option value="Remote">Remote</option>
                 <option value="Internship">Internship</option>
               </select>
+              {errors.type && touched.type && <div>{errors.type}</div>}
             </div>
 
             <div className="mb-4">
               <label className="block text-gray-700 font-bold mb-2"
-              >Job Listing Name</label
-              >
+              >Job Listing Name</label>
               <input
                 type="text"
                 id="title"
                 name="title"
-                className="border rounded w-full py-2 px-3 mb-2"
+                className={classInput}
                 placeholder="eg. Beautiful Apartment In Miami"
-                required
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                value={values.title}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
+              {errors.title && touched.title && <div className="text-red-500 text-xs">{errors.title}</div>}
             </div>
+
             <div className="mb-4">
               <label
                 htmlFor="description"
                 className="block text-gray-700 font-bold mb-2"
-              >Description</label
-              >
+              >Description</label>
               <textarea
                 id="description"
                 name="description"
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="Add any job duties, expectations, requirements, etc"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                value={values.description}
+                onChange={handleChange}
+                onBlur={handleBlur}
               ></textarea>
             </div>
 
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
-              >Salary</label
-              >
+              >Salary</label>
               <select
                 id="salary"
                 name="salary"
                 className="border rounded w-full py-2 px-3"
-                required
-                value={salary}
-                onChange={(e) => setSalary(e.target.value)}
+                value={values.salary}
+                onChange={handleChange}
+                onBlur={handleBlur}
               >
                 <option value="Under $50K">Under $50K</option>
                 <option value="$50K - 60K">$50K - $60K</option>
@@ -136,79 +148,79 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 type='text'
                 id='location'
                 name='location'
-                className='border rounded w-full py-2 px-3 mb-2'
+                className={classInput}
                 placeholder='Company Location'
-                required
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-              />
+                value={values.location}
+                onChange={handleChange}
+                onBlur={handleBlur} />
+              {errors.location && touched.location && <div className="text-red-500 text-xs">{errors.location}</div>}
             </div>
 
             <h3 className="text-2xl mb-5">Company Info</h3>
 
             <div className="mb-4">
-              <label htmlFor="company" className="block text-gray-700 font-bold mb-2"
-              >Company Name</label
-              >
+              <label htmlFor="companyName" className="block text-gray-700 font-bold mb-2"
+              >Company Name</label>
               <input
                 type="text"
-                id="company"
-                name="company"
+                id="companyName"
+                name="companyName"
                 className="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
-                value={companyName}
-                onChange={(e) => setCompanyName(e.target.value)}
+                value={values.companyName}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </div>
 
             <div className="mb-4">
               <label
-                htmlFor="company_description"
+                htmlFor="companyDescription"
                 className="block text-gray-700 font-bold mb-2"
-              >Company Description</label
-              >
+              >Company Description</label>
               <textarea
-                id="company_description"
-                name="company_description"
+                id="companyDescription"
+                name="companyDescription"
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="What does your company do?"
-                value={companyDescription}
-                onChange={(e) => setCompanyDescription(e.target.value)}
+                value={values.companyDescription}
+                onChange={handleChange}
+                onBlur={handleBlur}
               ></textarea>
             </div>
 
             <div className="mb-4">
               <label
-                htmlFor="contact_email"
+                htmlFor="contactEmail"
                 className="block text-gray-700 font-bold mb-2"
-              >Contact Email</label
-              >
+              >Contact Email</label>
               <input
                 type="email"
-                id="contact_email"
-                name="contact_email"
-                className="border rounded w-full py-2 px-3"
+                id="contactEmail"
+                name="contactEmail"
+                className={classInput}
                 placeholder="Email address for applicants"
-                required
-                value={contactEmail}
-                onChange={(e) => setContactEmail(e.target.value)}
+                value={values.contactEmail}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
+              {errors.contactEmail && touched.contactEmail && <div className="text-red-500 text-xs">{errors.contactEmail}</div>}
             </div>
+
             <div className="mb-4">
               <label
-                htmlFor="contact_phone"
+                htmlFor="contactPhone"
                 className="block text-gray-700 font-bold mb-2"
-              >Contact Phone</label
-              >
+              >Contact Phone</label>
               <input
-                type="tel"
-                id="contact_phone"
-                name="contact_phone"
-                className="border rounded w-full py-2 px-3"
+                type="number"
+                id="contactPhone"
+                name="contactPhone" className="border rounded w-full py-2 px-3"
                 placeholder="Optional phone for applicants"
-                value={contactPhone}
-                onChange={(e) => setContactPhone(e.target.value)}
+                value={values.contactPhone}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </div>
 
